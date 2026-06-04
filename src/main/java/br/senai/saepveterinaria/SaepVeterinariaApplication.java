7fd63eadd9083e909f78a02bd3e47ec9ce43a03f
Gabriel Henrique Francisco <gfrancisco.dev@gmail.com>
2026-06-04 20:54:21 -0300

    Iniciando o projeto

---
+++ src/main/java/br/senai/saepveterinaria/SaepVeterinariaApplication.java
@@ -0,0 +1,13 @@
+package br.senai.saepveterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaepVeterinariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaepVeterinariaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/senai/saepveterinaria/SaepVeterinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.saepveterinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaepVeterinariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
