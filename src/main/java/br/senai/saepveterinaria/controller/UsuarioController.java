06192633d71daef73cf39e29ccdcab456ca31ff9
Gabriel Henrique Francisco <gfrancisco.dev@gmail.com>
2026-06-12 10:00:32 -0300

    feat: Implementando as rotas de usuário

--- src/main/java/br/senai/saepveterinaria/controller/UsuarioController.java
+++ src/main/java/br/senai/saepveterinaria/controller/UsuarioController.java
@@ -1,13 +1,16 @@
 package br.senai.saepveterinaria.controller;
 
+import br.senai.saepveterinaria.dto.produto.ProdutoRequestDTO;
+import br.senai.saepveterinaria.dto.produto.ProdutoResponseDTO;
+import br.senai.saepveterinaria.dto.usuario.UsuarioRequestDTO;
 import br.senai.saepveterinaria.dto.usuario.UsuarioResponseDTO;
 
 import br.senai.saepveterinaria.service.UsuarioService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +24,33 @@ public class UsuarioController {
     @GetMapping("/listar")
     public ResponseEntity<List<UsuarioResponseDTO>> buscarTodos() {
         return ResponseEntity.ok(usuarioService.listarTodos());
+    }
+
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<UsuarioResponseDTO> buscarPorId(@PathVariable Integer id) {
+        return ResponseEntity.ok(usuarioService.listarPorId(id));
+    }
+
+    @GetMapping("/listar/email/{email}")
+    public ResponseEntity<UsuarioResponseDTO> buscarPorEmail(@PathVariable String email) {
+        return ResponseEntity.ok(usuarioService.listarPorEmail(email));
+    }
+
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<UsuarioResponseDTO> cadastrar(@RequestBody @Valid UsuarioRequestDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.cadastrar(dto));
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<UsuarioResponseDTO> atualizar(@PathVariable Integer id, @RequestBody UsuarioRequestDTO dto) {
+        return ResponseEntity.ok(usuarioService.atualizar(id, dto));
+    }
+
+    @DeleteMapping("/remover/{id}")
+    public ResponseEntity<String> remover(@PathVariable Integer id) {
+        usuarioService.remover(id);
+        return ResponseEntity.ok("Usuário removido com sucesso!");
     }
 
 }
--- src/main/java/br/senai/saepveterinaria/repository/UsuarioRepository.java
+++ src/main/java/br/senai/saepveterinaria/repository/UsuarioRepository.java
@@ -3,10 +3,11 @@ package br.senai.saepveterinaria.repository;
 import br.senai.saepveterinaria.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    Optional<Usuario> findByStatusUsuarioTrue();
+    List<Usuario> findByStatusUsuarioTrue();
     Optional<Usuario> findByEmailAndStatusUsuarioTrue(String email);
     Optional<Usuario> findByIdUsuarioAndStatusUsuarioTrue(Integer id);
     boolean existsByEmail(String email);
