8480da36e4ca0268826b4ec32ae6413b564e2ce1
Gabriel Henrique Francisco <gfrancisco.dev@gmail.com>
2026-09-04 15:14:18 -0300

    fix: corrige a configuração de CORS para incluir o frontend da Vercel

--- backend/src/main/java/br/senai/saepveterinaria/config/CorsConfig.java
+++ backend/src/main/java/br/senai/saepveterinaria/config/CorsConfig.java
@@ -19,7 +19,8 @@ public class CorsConfig {
         // Frontends autorizados
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "http://localhost:3000"
+                "http://localhost:3000",
+                "https://clinica-veterinaria-frontend-nine.vercel.app/"
         ));
 
         // Métodos HTTP permitidos
